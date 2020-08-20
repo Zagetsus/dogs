24efc310310ab23ae53b3bc0419aeff60a2a2f5f
Verdelho <luan@houpa.app>
2020-08-20 18:53:49 -0300

    Inicio de conf do login

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Styles from './Footer.module.css'
+// import { Container } from './styles';
+
+const Footer = () => {
+  return <div className={Styles.header} >Footer</div>;
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Styles from "./Header.module.css";
+import { Link } from "react-router-dom";
+import { ReactComponent as Dogs } from "../Assets/dogs.svg";
+
+function Header() {
+  return (
+    <header className={Styles.header}>
+      <nav className={`${Styles.nav} container`}>
+        <Link className={Styles.logo} to="/" aria-label="Dogs - Home">
+          <Dogs />
+        </Link>
+        <Link className={Styles.login} to="/login">Login / Criar</Link>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
