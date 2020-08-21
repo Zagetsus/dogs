7e7193b0c33671aa27d435e05cc477679584c09d
Verdelho <luan@houpa.app>
2020-08-21 18:36:05 -0300

    validação em formulário

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
+import useForm from "../../Hooks/useForm";
 
 const LoginForm = () => {
+
+  const username = useForm('email');
+  const password = useForm();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(username.value,),
+    })
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((json) => {
+        console.log(json);
+      });
+  }
+
   return (
     <div>
-      
-    </div>
-  )
-}
+      <h1>Login</h1>
 
-export default LoginForm
+      <form onSubmit={handleSubmit}>
+
+        <Input label="Usuário" type="text" name="username" {...username}/>
+        <Input label="Senha" type="password" name="password"  {...password}/>
+        <Button>Entrar</Button>
+
+      </form>
+
+      <Link to="/login/criar">Cadastro</Link>
+    </div>
+  );
+};
+
+export default LoginForm;
